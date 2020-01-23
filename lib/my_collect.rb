d9924bedb933c1c00152a4b1005c5c15b39ca411
Zaire  Swailes <swaileszaire@gmail.com>
2020-01-23 12:01:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,7 @@
 def my_collect
   array = ["John Smith", "Clark Kent", "Stephen Jameson", "James Jackson"]
   my_collect(array)
-  yield do |name| 
+  yield do |reverse| 
     while name.reverse("")
   end
 end
